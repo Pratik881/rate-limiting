5cb8675707947e41e2d22065793eb8fd2e7d5e86
Pratik <pratikn484@gmail.com>
2024-02-10 00:06:30 +0545

    nice

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ const limiter=rateLimit({
 })
 app.use(limiter)
 app.set('trust-proxy',true)
+app.use(express.static('public'))
 app.use(routes)
 require('dotenv').config()
 const PORT=process.env.PORT || 5000
